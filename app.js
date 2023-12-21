bf5dd24c927caa3a16eb5241933c6f0254c7b963
benjibennett12 <benjaminbennett0412@gmail.com>
2023-12-21 12:59:14 -0500

    server for our Hope Hacks website

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express(); // Thank you Nicole for downloading express
+const port = 5000;
+const path = require('path')
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//This will the home page where our astronomy api update will be on the home page 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'templates', 'index.html' ));
+});
+//This will be the Quiz Style / Image/Video Page
+app.get('/explore', (req, res) => {
+    res.sendFile(path.join(__dirname, 'templates', 'explore.html' ));
+   //might need to add the html file that this pat will exist on 
+})
+//This is a Tenative Idea for following currently that is on Mars
+app.get('/quiz', (req , res) => {
+    res.sendFile(path.join(__dirname, 'templates', 'quiz.html' ));
+  //need to add a html file for where this path will exist
+})
+
+app.get ('/subscribe', (req , res) => {
+    res.sendFile(path.join(__dirname, 'templates', 'subscribe.html' ));
+
+})
+
+app.listen(port , (err) => {
+   if(err) throw err;
+   console.log(`Server is working on ${port}`)
+})
+
+//I moved the server outside to my root in order for the HTML render
