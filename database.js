b8b8590c517b581344378b5bd5e8380f627d27a0
brynnaj <brynnaj78@gmail.com>
2024-01-02 22:46:27 -0500

    fixed database

--- database.js
+++ database.js
@@ -1,16 +1,33 @@
-const mysql = require('mysql2'); //import my sql module
 
-//initialize connection
-const connection = mysql.createConnection({
-host: 'localhost',
- database: 'HopeHacks',
- user: 'root', 
- password: 'password',
+
+const mysql = require('mysql2');
+
+// Create a connection pool
+const pool = mysql.createPool({
+  host: 'hopehacksdb.cvcq060maehe.us-east-2.rds.amazonaws.com',
+  database: 'HopeHacks',
+  user: 'admin',
+  password: 'password',
+  waitForConnections: true,
+  connectionLimit: 20,
+  queueLimit: 0 
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log('MySQL Database is Connected!!!!');
-   });
+// Export the pool for use in other modules
+module.exports = pool;
 
-module.exports = connection
+// get a connection from the pool whenever needed
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error getting connection from pool: ', err);
+    return;
+  }
+
+  console.log('MySQL Database is Connected!!!!');
+
+  // Do your database operations using the 'connection' object
+
+  // Release the connection back to the pool when done
+  connection.release();
+});
+
